0601404e9afd004e8f6168712f8a221d94ea2811
Nicholas Grasevski <nicholas.grasevski@rokt.com>
2021-04-24 10:52:07 +1000

    add accuracy

--- Makefile
+++ Makefile
@@ -7,11 +7,14 @@ $(STUDENTIDS).zip: report/report.pdf
 	cd $(STUDENTIDS) && zip -r ../$@ *
 	rm -r $(STUDENTIDS)
 
-report/report.pdf: report/report.tex results.csv model.png
-	cd report && pdflatex report.tex
+report/report.pdf: report/report.tex report/references.bib report/results.csv report/model.png
+	cd report && pdflatex --synctex=1 report.tex && biber report && pdflatex --synctex=1 report.tex && pdflatex --synctex=1 report.tex
 
-results.csv: assignment1.py
-	./assignment1.py >results.csv
+report/results.csv: assignment1.py
+	./assignment1.py >$@
 
 lint:
 	yapf -d assignment1.py && pycodestyle assignment1.py
+
+fix:
+	yapf -i assignment1.py
--- assignment1.py
+++ assignment1.py
@@ -4,6 +4,8 @@ import csv
 import itertools
 import json
 import numpy as np
+from typing import Tuple
+from scipy.sparse import coo_matrix
 import sys
 
 
@@ -21,6 +23,44 @@ def softmax(X: np.ndarray, axis: int) -> np.ndarray:
 def log_loss(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """Multiclass cross entropy cost."""
     return -(y_true * np.log(y_pred)).sum(axis=1).mean()
+
+
+def acc(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Top-1 accuracy."""
+    return (y_true.argmax(axis=1) == y_pred.argmax(axis=1)).mean()
+
+
+def f1_macro(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Harmonic mean of precision and recall."""
+    y_true, y_pred = y_true.argmax(axis=1), y_pred.argmax(axis=1)
+    c = (np.ones_like(y_true), (y_true, y_pred))
+    c = coo_matrix(c, shape=(CLASSES, CLASSES)).toarray()
+    d = np.diag(c)
+    precision, recall = d / c.sum(axis=1), d / c.sum(axis=0)
+    return 2 * (precision * recall / (precision + recall)).mean()
+
+
+def binary_roc_auc(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """One vs rest ROC AUC."""
+    ix = np.argsort(y_pred)[::-1]
+    y_true, y_pred = y_true[ix], y_pred[ix]
+    distinct_value_indices = np.where(np.diff(y_pred))[0]
+    threshold_idxs = np.r_[distinct_value_indices, y_true.size - 1]
+    tps = y_true.cumsum()[threshold_idxs]
+    fps = 1 + threshold_idxs - tps
+    thresholds = y_pred[threshold_idxs]
+    c = np.logical_or(np.diff(fps, 2), np.diff(tps, 2))
+    optimal_idxs = np.where(np.r_[True, c, True])[0]
+    fps, tps = fps[optimal_idxs], tps[optimal_idxs]
+    fps, tps = np.r_[0, fps], np.r_[0, tps]
+    fpr, tpr = fps / fps[-1], tps / tps[-1]
+    return np.trapz(tpr, fpr)
+
+
+def roc_auc_macro(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """Area Under the Receiver Operating Characteristic Curve."""
+    res = [binary_roc_auc(y_true[:, c], y_pred[:, c]) for c in classes]
+    return np.mean(res)
 
 
 class Dense:
@@ -50,14 +90,13 @@ class Dense:
             ret *= self._D
         return ret
 
-    def backward(self, delta: np.ndarray) -> np.ndarray:
+    def backward(self, delta: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """Calculate gradient for linear, activation and dropout."""
         delta *= self._D
         if self._activation == 'relu':
             delta *= self._next_X > 0
-        self.grad_W = (self._X.T @ delta) / len(delta) + self._l2 * self.W
-        self.grad_b = delta.mean(axis=0)
-        return delta @ self.W.T
+        grad_W = (self._X.T @ delta) / len(delta) + self._l2 * self.W
+        return delta @ self.W.T, grad_W
 
 
 class BatchNorm:
@@ -81,13 +120,12 @@ class BatchNorm:
         self._X = (X - mean) * self._std_inv
         return self.W * self._X + self.b
 
-    def backward(self, delta: np.ndarray) -> np.ndarray:
+    def backward(self, delta: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """Calculate gradient for batch normalization."""
-        self.grad_W = (delta * self._X).mean(axis=0)
-        self.grad_b = delta.mean(axis=0)
         dx = delta * self.W
+        grad_W = (delta * self._X).mean(axis=0)
         return self._std_inv * (dx - dx.mean(axis=0) - self._X *
-                                (dx * self._X).mean(axis=0))
+                                (dx * self._X).mean(axis=0)), grad_W
 
 
 class Classifier:
@@ -99,6 +137,8 @@ class Classifier:
     def fit(self,
             X: np.ndarray,
             y: np.ndarray,
+            X_test: np.ndarray,
+            y_test: np.ndarray,
             batch_size: int,
             epochs: int,
             lr: float,
@@ -121,16 +161,21 @@ class Classifier:
                 y_true = batch[:, -y.shape[1]:]
                 cost, delta = log_loss(y_true, y_pred), y_pred - y_true
                 for i, layer in reversed(list(enumerate(self._layers))):
-                    delta = layer.backward(delta)
+                    grad_b = delta.mean(axis=0)
+                    delta, grad_W = layer.backward(delta)
                     v_W[i] *= momentum
-                    v_W[i] += lr * layer.grad_W
+                    v_W[i] += lr * grad_W
                     v_b[i] *= momentum
-                    v_b[i] += lr * layer.grad_b
+                    v_b[i] += lr * grad_b
                     layer.W -= v_W[i]
                     layer.b -= v_b[i]
                 loss += len(batch) * cost / len(data)
             progress['epoch'] = epoch + 1
             progress['logloss'] = loss
+            for (t, X_val, y_val) in ('train', X, y), ('test', X_test, y_test):
+                y_pred = self.predict_proba(X_val)
+                for m in metrics:
+                    progress[f'{t}_{m.__name__}'] = m(y_val, y_pred)
             print(json.dumps(progress), file=sys.stderr)
             if loss > best or np.isnan(loss):
                 break
@@ -147,17 +192,21 @@ class Classifier:
     np.load(f'data/{k}.npy')
     for k in ('train_data', 'train_label', 'test_data', 'test_label')
 ]
-classes = list(range(10))
+CLASSES = 10
+classes = list(range(CLASSES))
 y = label_binarize(y, classes=classes)
 y_test = label_binarize(y_test, classes=classes)
 combinations = itertools.product([0, 0.2], [0, 0.01], [0, 0.9])
-fields = [
-    'Dropout', 'L2', 'Momentum', 'Train', 'Train std', 'Test', 'Test std'
+metrics = [log_loss, acc, f1_macro, roc_auc_macro]
+fields = ['Dropout', 'L2', 'Momentum'] + [
+    f'{t} {m.__name__}{s}' for t in ['Train', 'Test'] for m in metrics
+    for s in ['', ' std']
 ]
 w = csv.DictWriter(sys.stdout, fieldnames=fields)
 w.writeheader()
 trials = 5
-train, test = np.zeros(trials), np.zeros(trials)
+train = np.zeros((trials, len(metrics)))
+test = np.zeros((trials, len(metrics)))
 for dropout, l2, momentum in combinations:
     row = {'Dropout': dropout, 'L2': l2, 'Momentum': momentum}
     for i in range(trials):
@@ -169,13 +218,15 @@ for dropout, l2, momentum in combinations:
             BatchNorm(64, 0.99, 0.001),
             Dense(64, y.shape[1]),
         ])
-        model.fit(X, y, 1000, 50, 0.1, momentum=momentum)
+        model.fit(X, y, X_test, y_test, 1000, 50, 0.1, momentum=momentum)
         y_pred = model.predict_proba(X)
-        train[i] = log_loss(y, y_pred)
-        y_pred = model.predict_proba(X_test)
-        test[i] = log_loss(y_test, y_pred)
-    row['Train'] = train.mean()
-    row['Train std'] = train.std()
-    row['Test'] = test.mean()
-    row['Test std'] = test.std()
+        y_test_pred = model.predict_proba(X_test)
+        for k, m in enumerate(metrics):
+            train[i][k] = m(y, y_pred)
+            test[i][k] = m(y_test, y_test_pred)
+    for t, a in ('Train', train), ('Test', test):
+        mean, std = a.mean(axis=0), a.std(axis=0)
+        for n, m, s in zip(metrics, mean, std):
+            row[f'{t} {m.__name__}'] = m
+            row[f'{t} {m.__name__} std'] = s
     w.writerow(row)
